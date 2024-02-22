4e3e1ea3283c12977cc1de3e49441e05c31b0a58
Gokul Krishna S <gokuls759@gmail.com>
2024-02-22 17:29:06 +0000

    added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="visa-approval-prediction",
+    version="0.0.0",
+    author="Gokul",
+    author_email="gokuls759@gmail.com",
+    packages=find_packages()
+)
